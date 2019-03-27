25e13c3a1a69fe423eb2174d05e268a8403e8037
Tolik Dem <tolik.demianov@gmail.com>
2019-03-27 10:20:43 +0300

    написал js для выпадающего меню

--- src/main.js
+++ src/main.js
@@ -3,5 +3,30 @@ import './main.scss';
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log('test my build');
+
+  // обработчик клика для раскрывающихся меню в панели навигации
+  const navClickHandler = (e) => {
+    const t = e.target;
+    const ct = e.currentTarget;
+    const droppedMenu = ct.querySelector('.dropdown-menu');
+    const arrow = ct.querySelector('.nav-expanded-item__arrow');
+    if (t.classList.contains('nav-li') ||
+        t.classList.contains('svg-nav-arrow') ||
+        t.tagName == "IMG" ||
+        t.tagName == "svg" || 
+        t.tagName == "path") {
+        droppedMenu.classList.toggle('dropdown-menu--hidden');
+        arrow.classList.toggle('nav-expanded-item__arrow--reversed');
+    }
+  }
+
+  // получить все елементы nav-expanded-item
+  const allExpanded = document.querySelectorAll('.nav-expanded-item');
+  if (allExpanded !== null && allExpanded.length > 0) {
+    console.log('allExpanded', allExpanded);
+    // на каждый элемент навесить обработчик клика
+    Array.prototype.forEach.call(allExpanded, (btn) => {
+      btn.addEventListener('click', (e) => navClickHandler(e));
+    });
+  }
 });
