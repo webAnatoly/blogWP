6f07351ba660a5fc12c73d03362e7f49978bd8c5
Tolik Dem <tolik.demianov@gmail.com>
2019-03-29 21:12:25 +0300

    написал функцию для автоувеличения высоты textarea во время ввода текста

--- src/main.js
+++ src/main.js
@@ -29,4 +29,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
       btn.addEventListener('click', (e) => navClickHandler(e));
     });
   }
+
+  // сделать, чтобы текстареа раздвигалась в процессе набора текста
+  const textarea = document.getElementById('commentFormTextarea');
+  const autoExpand = (field) => {
+    // Reset field height
+    field.style.height = 'inherit';
+
+    // Get the computed styles for the element
+    const computed = window.getComputedStyle(field);
+
+    // Calculate the height
+    const height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+                + parseInt(computed.getPropertyValue('padding-top'), 10)
+                + field.scrollHeight
+                + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+                + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+
+    field.style.height = height + 'px';
+  } // end of autoExpand
+  textarea.addEventListener('input', function (e) {
+    // if (e.target.tagName.toLowerCase() !== 'textarea') return;
+    autoExpand(e.target);
+  }, false);
+  // подсветка textarea при фокусе
+  textarea.addEventListener('focus', (e) => {
+    e.target.style.border = '1px solid #ffcf34';
+  });
+  textarea.addEventListener('blur', (e) => {
+    e.target.style.border = '1px solid #d9d9d9';
+  });
 });
